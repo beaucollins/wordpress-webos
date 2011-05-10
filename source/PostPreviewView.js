0495743f65e7033554daae494d65ff41c77bcf55
daniloercoli <daniloercoli@ee5cff3a-74c1-40a4-9fec-33ab0844ee87>
2011-05-10 14:27:50 +0000

    remote post preview improvements
    git-svn-id: https://webos.svn.wordpress.org/trunk@43 ee5cff3a-74c1-40a4-9fec-33ab0844ee87

--- source/PostPreviewView.js
+++ source/PostPreviewView.js
@@ -11,30 +11,50 @@ enyo.kind({
 	 this.inherited(arguments);
   },
   components: [
-    //{ kind:'PalmService', service:'palm://com.palm.applicationManager/', method:'open' },
-	{name: "postPreviewService", kind: "WebService",
-        method: "POST",
-        handleAs: "text",
-        contentType: 'application/x-www-form-urlencoded',
-        onSuccess: "onPreviewSuccess",
-        onFailure: "onPreviewFailure"},
-      {name: "postPreviewResponse", kind: "HtmlContent"}
+     {name: "postPreviewResponse", kind: "HtmlContent"}
   ],
   openPostURL:function(){
+	  //TODO: check the connection to host here!!!
 	  if (this.post.permaLink && this.post.permaLink.trim() != "" ) {
-		  console.log(this.account);
-		  console.log(this.post);
 		  var loginURL = this.account.xmlrpc.replace("/xmlrpc.php", "/wp-login.php");
 		  var postdata='log='+this.account.username+'&pwd='+this.account.password+'&redirect_to='+this.post.permaLink;
-		  console.log("post permalink: "+ this.post.permaLink+"  login URL: "+ loginURL);
-		  console.log("login info: " +postdata);
-		  this.$.postPreviewService.url = loginURL;
-		  this.$.postPreviewService.call({
-			  postBody: postdata
-		  });
+		  var htmlForm ='<h1>Loading Preview...</h1><form method="post" action="'+loginURL+'" id="loginform" name="loginform" style="visibility:hidden">'
+		  +'<input type="text" tabindex="10" size="20" value="'+this.account.username+'" class="input" id="user_login" name="log"></label>'
+		  +'<input type="password" tabindex="20" size="20" value="'+this.account.password+'" class="input" id="user_pass" name="pwd"></label>'
+		  +'<input type="submit" tabindex="100" value="Log In" class="button-primary" id="wp-submit" name="wp-submit">'
+		  +'<input type="hidden" value="'+ this.post.permaLink +'" name="redirect_to">'
+		  +'</form>';
+		  this.$.postPreviewResponse.setContent(htmlForm);
+
+		  loginform.submit();
+	  } else {
+		//fallback to  local preview	    
+		  var alert_msg = "Sorry, something went wrong during preview. A simple preview is shown below.";
+		  this.loadlocalPreview(alert_msg, this.post.title,  this.post.description, this.post.mt_keywords, this.post.categories); 
 	  }
   },
   loadlocalPreview:function(alert_msg, title, content, tags, categories){
+	  
+	  if (typeof(title) == "undefined" || enyo.string.trim(title) =="")
+		  title = "(no title)";		  
+	  
+	  if (typeof(content) == "undefined" || enyo.string.trim(content) == "")
+		  content = "No Description available for this Post";
+	  
+	  if(tags && enyo.string.trim(tags) != "") {
+		  tags ='Tags: '+ tags;
+	  } else {
+		  tags = "";
+	  }
+	  
+	  if(categories && categories.length > 0) {
+		  categories ='Categories: '+ this.post.categories.join(', ');
+	  } else {
+		  categories ="";
+	  }
+	  
+	  var alert_msg = "Sorry, no connection to host. A simple preview is shown below.";
+	  
 	  var content = '<div class="preview_page"><h5 id="preview_alert">'+alert_msg+'</h5>'+
 	  '<h1>'+ title + '</h1>'+
 	  '<div id="preview_content">' +
@@ -44,44 +64,17 @@ enyo.kind({
 	  '</div></div>';
 	  this.$.postPreviewResponse.setContent(content);
   },
-  onPreviewSuccess: function(inSender, inResponse) {
-	  this.$.postPreviewResponse.setContent(inResponse);
-	  console.log("success response = " + inResponse);
-  },
-  onPreviewFailure: function(inSender, inResponse) {
-	  console.log("failure response = " + inResponse);
-	  //fallback to  local preview	  
-	  
-	  var title = this.post.title;
-	  if (typeof(title) == "undefined" || enyo.string.trim(title) =="")
-		  title = "(no title)";		  
-	  
-	  var content = this.post.description;
-	  if (typeof(content) == "undefined" || enyo.string.trim(content) == "")
-		  content = "No Description available for this Post";
-	  
-	  var tags = this.post.mt_keywords;
-	  var categories = this.post.categories;
-	  var alert_msg = "Sorry, no connection to host. A simple preview is shown below.";
-	  
-	  this.loadlocalPreview(alert_msg, title,content, tags, categories); 
-  },
   windowParamsChangeHandler: function() {
 	  if(typeof(enyo.windowParams.account) == "undefined") {
 		  //load local preview	  
-		  var title = enyo.windowParams.title;
-		  if (typeof(title) == "undefined" || enyo.string.trim(title) =="")
-			  title = "(no title)";		  
+		  var title = enyo.windowParams.title; 
 		  var content = enyo.windowParams.content;
-		  if (typeof(content) == "undefined" || enyo.string.trim(content) == "")
-			  content = "No Description available for this Post";
 		  var tags = "";
 		  var categories = "";
 		  var alert_msg = "Sorry, the post has changed, or it is not published. A simple preview is shown below.";
 		  this.loadlocalPreview(alert_msg, title,content, tags, categories);
 	  } else {
 		  //load remote preview
-		  this.$.postPreviewResponse.setContent("<h1>Loading Preview...</h1>");
 		  this.account = enyo.windowParams.account;
 		  this.post = enyo.windowParams.post;
 		  this.openPostURL();
