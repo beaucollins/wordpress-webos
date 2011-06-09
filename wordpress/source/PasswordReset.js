c9d1a367920b6d2623218a1978ded1893de9620f
beaucollins <beaucollins@ee5cff3a-74c1-40a4-9fec-33ab0844ee87>
2011-06-09 19:08:14 +0000

    password reset form now has username/blogname
    git-svn-id: https://webos.svn.wordpress.org/trunk@209 ee5cff3a-74c1-40a4-9fec-33ab0844ee87

--- wordpress/source/PasswordReset.js
+++ wordpress/source/PasswordReset.js
@@ -2,6 +2,7 @@ enyo.kind({
   name:'PasswordReset',
   kind:'enyo.Popup',
   scrim:true,
+  lazy:false,
   events: {
     onSavePassword:'',
     onCancel:''
@@ -12,6 +13,7 @@ enyo.kind({
   components:[
     { content:$L("Bad username/password") },
     //{ name:'username' },
+    { name:'message', style:'font-size:14px; padding:5px 0;' },
     { name:'login', kind:'enyo.RowGroup', caption:$L('Password'), components:[
       { kind:'enyo.Input', name:'passwordField', inputType:'password' },
     ]},
@@ -19,7 +21,12 @@ enyo.kind({
     { kind:'enyo.Button', content:$L('Cancel'), onclick:'doCancel' }
   ],
   accountChanged:function(){
+    console.log("Account changed", this.account);
     // this.$.username.setContent(this.account.account.username);
+    this.$.message.setContent(new enyo.g11n.Template($L('Password for #{username} on blog #{blogName}')).evaluate({
+      username: this.account.account.username,
+      blogName: this.account.account.blogName
+    }));
   },
   savePassword:function(sender){
     this.doSavePassword(this.$.passwordField.getValue());
